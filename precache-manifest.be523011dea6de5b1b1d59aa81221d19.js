c33ae9e5b07b8088c25967ee850af66961a454a2
sarfarazkasmani <sarfarazkasmani@gmail.com>
2019-10-23 18:00:39 +0530

    Updates

---
+++ precache-manifest.be523011dea6de5b1b1d59aa81221d19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b839f9dc6938d140a1b977a295de03b5",
+    "url": "/react-hooks-todoapp/index.html"
+  },
+  {
+    "revision": "8d79e45be11c00e9f205",
+    "url": "/react-hooks-todoapp/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "742cc1ed83e58c428779",
+    "url": "/react-hooks-todoapp/static/js/2.64ceb7e2.chunk.js"
+  },
+  {
+    "revision": "8d79e45be11c00e9f205",
+    "url": "/react-hooks-todoapp/static/js/main.aa51bbbb.chunk.js"
+  },
+  {
+    "revision": "31fe6555e3d5f8d4ab21",
+    "url": "/react-hooks-todoapp/static/js/runtime-main.7ad23280.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react-hooks-todoapp/static/media/logo.25bf045c.svg"
+  }
+]);
